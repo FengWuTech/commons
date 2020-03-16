0e7e12001cca903919692f3c34b0c6f02514a837
codingdogs <codingdogs@163.com>
2020-03-16 16:49:23 +0800

    添加常量

---
+++ constant/bank_constant.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	SUZHOU_BANK = 1
+)
+
+var BANK_MAP = map[int]string{
+	SUZHOU_BANK: "苏州银行",
+}
---
+++ constant/union_pay_constant.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	UNION_PAY_SUZHOU_BANK = 1
+)
+
+var UNION_PAY_MAP = map[int]string{
+	UNION_PAY_SUZHOU_BANK: "苏州银行聚合支付中心",
+}
