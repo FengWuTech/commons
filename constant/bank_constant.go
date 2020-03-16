55f72ccd356c920590bde1628165ef82ae8a70e2
codingdogs <codingdogs@163.com>
2020-03-16 16:56:18 +0800

    update

--- constant/bank_constant.go
+++ constant/bank_constant.go
@@ -4,6 +4,14 @@ const (
 	SUZHOU_BANK = 1
 )
 
-var BANK_MAP = map[int]string{
-	SUZHOU_BANK: "苏州银行",
-}
+var (
+	BANK_MAP = map[int]string{
+		SUZHOU_BANK: "苏州银行",
+	}
+	BANK_LIST = []map[string]interface{}{
+		{
+			"value": SUZHOU_BANK,
+			"label": "苏州银行",
+		},
+	}
+)
--- constant/union_pay_constant.go
+++ constant/union_pay_constant.go
@@ -4,6 +4,14 @@ const (
 	UNION_PAY_SUZHOU_BANK = 1
 )
 
-var UNION_PAY_MAP = map[int]string{
-	UNION_PAY_SUZHOU_BANK: "苏州银行聚合支付中心",
-}
+var (
+	UNION_PAY_MAP = map[int]string{
+		UNION_PAY_SUZHOU_BANK: "苏州银行聚合支付中心",
+	}
+	UNION_PAY_LIST = []map[string]interface{}{
+		{
+			"value": UNION_PAY_SUZHOU_BANK,
+			"label": "苏州银行聚合支付中心",
+		},
+	}
+)
