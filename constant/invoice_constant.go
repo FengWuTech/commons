1d149132066af9a747b0dfa01a0ef2547ebf2934
杨扬 <yangyang@aiforward.com>
2020-03-19 18:53:22 +0800

    修改发票常量

--- constant/invoice_constant.go
+++ constant/invoice_constant.go
@@ -4,14 +4,14 @@ import "sort"
 
 const (
 	INVOICE_STATUS       = 0
-	INVOICE_STATUS_START = 0 //申请中
-	INVOICE_STATUS_DONE  = 1 //完成
-	INVOICE_STATUS_FAIL  = 2 //失败
+	INVOICE_STATUS_START = 1 //申请中
+	INVOICE_STATUS_DONE  = 2 //完成
+	INVOICE_STATUS_FAIL  = 3 //失败
 
 	INVOICE_TYPE                 = 1
-	INVOICE_TYPE_PERSONAL        = 0 //个人
-	INVOICE_TYPE_COMPANY         = 1 //公司普票
-	INVOICE_TYPE_COMPANY_SPECIAL = 2 //公司专票
+	INVOICE_TYPE_PERSONAL        = 1 //个人
+	INVOICE_TYPE_COMPANY         = 2 //公司普票
+	INVOICE_TYPE_COMPANY_SPECIAL = 3 //公司专票
 )
 
 var INVOICE_INFO_MAP = map[int]map[int]map[string]interface{}{
