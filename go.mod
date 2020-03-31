eb64a4432d6e43bf6250b6525699850ded9b8cce
杨扬 <yangyang@aiforward.com>
2020-03-31 16:57:06 +0800

    增加qiniu

--- go.mod
+++ go.mod
@@ -5,16 +5,21 @@ go 1.14
 require (
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.1.0
 	github.com/astaxie/beego v1.12.1
+	github.com/elazarl/goproxy v0.0.0-20200315184450-1f3cb6622dad // indirect
 	github.com/getsentry/sentry-go v0.5.1
 	github.com/gin-gonic/gin v1.6.2
 	github.com/go-ini/ini v1.55.0
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/json-iterator/go v1.1.9
 	github.com/noaway/dateparse v0.0.0-20171117034806-ad2b19d7b298
+	github.com/parnurzeal/gorequest v0.2.16
+	github.com/qiniu/api.v7/v7 v7.4.1
+	github.com/satori/go.uuid v1.2.0
 	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726
 	github.com/unknwon/com v1.0.1
 	go.uber.org/zap v1.14.1
 	gopkg.in/ini.v1 v1.55.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	moul.io/http2curl v1.0.0 // indirect
 )
---
+++ qiniu/sdk.go
@@ -0,0 +1,64 @@
+package qiniu
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/FengWuTech/commons/setting"
+	"github.com/parnurzeal/gorequest"
+	"github.com/qiniu/api.v7/v7/auth/qbox"
+	"github.com/qiniu/api.v7/v7/storage"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+func GetUploadToken() string {
+	bucket := setting.AppSetting.QBoxBucket
+	putPolicy := storage.PutPolicy{
+		Scope: bucket,
+	}
+	accessKey := setting.AppSetting.QBoxAccess
+	secretKey := setting.AppSetting.QBoxSecret
+	mac := qbox.NewMac(accessKey, secretKey)
+	upToken := putPolicy.UploadToken(mac)
+
+	return upToken
+}
+
+func UploadHttpFile(url string, dstFileName string) string {
+	var timenow = time.Now()
+	dstFileName = fmt.Sprintf("%04d%02d%02d/%s/%s", timenow.Year(), timenow.Month(), timenow.Day(), uuid.NewV4().String(), dstFileName)
+	bucket := setting.AppSetting.QBoxBucket
+	putPolicy := storage.PutPolicy{
+		Scope: bucket,
+	}
+	accessKey := setting.AppSetting.QBoxAccess
+	secretKey := setting.AppSetting.QBoxSecret
+	mac := qbox.NewMac(accessKey, secretKey)
+	upToken := putPolicy.UploadToken(mac)
+
+	cfg := storage.Config{
+		Zone: &storage.ZoneHuabei,
+	}
+	// 是否使用https域名
+	cfg.UseHTTPS = false
+	// 上传是否使用CDN上传加速
+	cfg.UseCdnDomains = false
+	// 构建表单上传的对象
+	formUploader := storage.NewFormUploader(&cfg)
+	ret := storage.PutRet{}
+
+	_, content, _ := gorequest.New().Get(url).End()
+	data := []byte(content)
+	dataLen := int64(len(data))
+	putExtra := storage.PutExtra{
+		Params: map[string]string{},
+	}
+	err := formUploader.Put(context.Background(), &ret, upToken, dstFileName, bytes.NewReader(data), dataLen, &putExtra)
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return ""
+	}
+	return setting.AppSetting.CdnURL + "/" + dstFileName
+}
