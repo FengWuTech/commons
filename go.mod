5471be3cf2998b849ef0a8247567f465f37caed8
杨扬 <yangyang@aiforward.com>
2020-03-31 11:51:20 +0800

    增加setting

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/FengWuTech/commons
 go 1.14
 
 require (
+	github.com/go-ini/ini v1.55.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/noaway/dateparse v0.0.0-20171117034806-ad2b19d7b298 // indirect
 	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
---
+++ setting/setting.go
@@ -0,0 +1,158 @@
+package setting
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-ini/ini"
+)
+
+type App struct {
+	PageSize  int
+	PrefixUrl string
+
+	RuntimeRootPath string
+
+	ImageSavePath  string
+	ImageMaxSize   int
+	ImageAllowExts []string
+
+	ExportSavePath string
+	QrCodeSavePath string
+	FontSavePath   string
+
+	LogSavePath string
+	LogSaveName string
+	LogFileExt  string
+	LogLevel    string
+	TimeFormat  string
+
+	DivisionPrecision int
+
+	CdnURL     string
+	QBoxBucket string
+	QBoxAccess string
+	QBoxSecret string
+
+	MinipStaffAppID         string
+	MinipStaffSecret        string
+	MinipStaffOriID         string
+	MinipStaffToken         string
+	MinipStaffEncodedAESKey string
+}
+
+var AppSetting = &App{}
+
+type Remote struct {
+	MinipUserBaseUrl  string
+	PayCenterBaseUrl  string
+	EsServiceUrl      string
+	MinipStaffBaseURL string
+	SentryDSN         string
+}
+
+var RemoteSetting = &Remote{}
+
+type Server struct {
+	RunMode      string
+	HttpPort     int
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+var ServerSetting = &Server{}
+
+type Database struct {
+	Type        string
+	User        string
+	Password    string
+	Host        string
+	Name        string
+	TablePrefix string
+}
+
+var DatabaseSetting = &Database{}
+
+type DatabaseFlow struct {
+	Type        string
+	User        string
+	Password    string
+	Host        string
+	Name        string
+	TablePrefix string
+}
+
+var DatabaseFlowSetting = &DatabaseFlow{}
+
+type Redis struct {
+	Host        string
+	Password    string
+	MaxIdle     int
+	MaxActive   int
+	IdleTimeout time.Duration
+}
+
+var RedisSetting = &Redis{}
+
+type Sso struct {
+	Host string
+}
+
+var SsoSetting = &Sso{}
+
+type Cas struct {
+	BaseURL string
+	AppID   string
+	ApiKey  string
+}
+
+var CasSetting = &Cas{}
+
+var cfg *ini.File
+
+// Setup initialize the configuration instance
+func Setup(env *string) {
+	var (
+		err      error
+		cfgFiles []string
+	)
+	if *env == "dev" {
+		cfgFiles = []string{"conf/dev.ini"}
+	} else if *env == "test" {
+		cfgFiles = []string{"conf/test.ini"}
+	} else {
+		panic("invalid env")
+	}
+	for _, cfgFile := range cfgFiles {
+		cfg, err = ini.Load(cfgFile)
+		if err != nil {
+			fmt.Printf("setting.Setup, fail to parse '%s': %v\n", cfgFile, err)
+		} else {
+			fmt.Printf("setting.Setup, parse '%s' success\n", cfgFile)
+			break
+		}
+	}
+
+	mapTo("app", AppSetting)
+	mapTo("server", ServerSetting)
+	mapTo("database", DatabaseSetting)
+	mapTo("redis", RedisSetting)
+	mapTo("sso", SsoSetting)
+	mapTo("database_flow", DatabaseFlowSetting)
+	mapTo("remote", RemoteSetting)
+	mapTo("cas", CasSetting)
+
+	AppSetting.ImageMaxSize = AppSetting.ImageMaxSize * 1024 * 1024
+	ServerSetting.ReadTimeout = ServerSetting.ReadTimeout * time.Second
+	ServerSetting.WriteTimeout = ServerSetting.WriteTimeout * time.Second
+	RedisSetting.IdleTimeout = RedisSetting.IdleTimeout * time.Second
+}
+
+// mapTo map section
+func mapTo(section string, v interface{}) {
+	err := cfg.Section(section).MapTo(v)
+	if err != nil {
+		fmt.Printf("Cfg.MapTo %s err: %v", section, err)
+	}
+}
+
