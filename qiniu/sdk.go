178e4a5f167673c01c027bb420acc0a5af1a4989
杨扬 <yangyang@aiforward.com>
2020-04-01 12:47:03 +0800

    Add upload

--- qiniu/sdk.go
+++ qiniu/sdk.go
@@ -62,3 +62,26 @@ func UploadHttpFile(url string, dstFileName string) string {
 	}
 	return setting.AppSetting.CdnUrl + "/" + dstFileName
 }
+
+func Upload(key string, localFile string) string {
+	putPolicy := storage.PutPolicy{
+		Scope: setting.AppSetting.QBoxBucket,
+	}
+	mac := qbox.NewMac(setting.AppSetting.QBoxAccess, setting.AppSetting.QBoxSecret)
+	upToken := putPolicy.UploadToken(mac)
+	cfg := storage.Config{}
+	// 空间对应的机房
+	cfg.Zone = &storage.ZoneHuabei
+	// 是否使用https域名
+	cfg.UseHTTPS = false
+	// 上传是否使用CDN上传加速
+	cfg.UseCdnDomains = false
+	// 构建表单上传的对象
+	formUploader := storage.NewFormUploader(&cfg)
+	ret := storage.PutRet{}
+	err := formUploader.PutFile(context.Background(), &ret, upToken, key, localFile, nil)
+	if err != nil {
+		return ""
+	}
+	return setting.AppSetting.CdnUrl + key
+}
