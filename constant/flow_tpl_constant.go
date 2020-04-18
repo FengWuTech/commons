41dccbd3c1159d5734eddb276ec73fbd019ce0bb
codingdogs <codingdogs@163.com>
2020-04-18 16:38:43 +0800

    添加文件说明

---
+++ constant/flow_tpl_constant.go
@@ -0,0 +1,12 @@
+package constant
+
+const (
+	FLOW_TPL_FILE_TYPE_CSV = 1
+)
+
+var FLOW_TPL_FILE_TYPE_MAP = []map[string]interface{}{
+	{
+		"label": ".csv",
+		"value": FLOW_TPL_FILE_TYPE_CSV,
+	},
+}
