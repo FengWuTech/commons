11a11548e3c246a090a1d066f0a49634ca6a642f
codingdogs <codingdogs@163.com>
2020-04-22 14:23:16 +0800

    添加常量

--- constant/urm_constant.go
+++ constant/urm_constant.go
@@ -1,11 +1,12 @@
 package constant
 
 const (
-	SMS_TPL_TYPE_NORMAL = 1 //普通短信
-	SMS_TPL_TYPE_AD     = 2 //广告
+	SMS_TPL_TYPE_VERIFY_CODE   = 0 //验证码
+	SMS_TPL_TYPE_NORMAL        = 1 //普通短信
+	SMS_TPL_TYPE_AD            = 2 //广告
+	SMS_TPL_TYPE_INTERNATIONAL = 3 //国际&港澳台
 
-	SMS_TPL_FAIL       = -1 //查询失败
-	SMS_TPL_AUDITING   = 0  //审核中
-	SMS_TPL_AUDIT_PASS = 1  //审核通过
-	SMS_TPL_AUDIT_FAIL = 2  //审核失败
+	SMS_TPL_AUDITING   = 0 //审核中
+	SMS_TPL_AUDIT_PASS = 1 //审核通过
+	SMS_TPL_AUDIT_FAIL = 2 //审核失败
 )
