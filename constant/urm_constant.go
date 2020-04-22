f31b74affe978091bcfb272480be8cf90cc5a081
codingdogs <codingdogs@163.com>
2020-04-22 14:32:06 +0800

    添加常量

--- constant/urm_constant.go
+++ constant/urm_constant.go
@@ -1,10 +1,12 @@
 package constant
 
 const (
-	SMS_TPL_TYPE_VERIFY_CODE   = 0 //验证码
-	SMS_TPL_TYPE_NORMAL        = 1 //普通短信
-	SMS_TPL_TYPE_AD            = 2 //广告
-	SMS_TPL_TYPE_INTERNATIONAL = 3 //国际&港澳台
+	SMS_TPL_TYPE_VERIFY_CODE = 0 //验证码
+	SMS_TPL_TYPE_NORMAL      = 1 //普通短信
+	SMS_TPL_TYPE_AD          = 2 //广告
+
+	SMS_TPL_REGION_DOMESTIC      = 0 //国内短信
+	SMS_TPL_REGION_INTERNATIONAL = 1 //港澳台和国际短信
 
 	SMS_TPL_AUDITING   = 0 //审核中
 	SMS_TPL_AUDIT_PASS = 1 //审核通过
