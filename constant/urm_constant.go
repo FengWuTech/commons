965ff6c26abb2b3e328ab60c13ca8de19aac6a6d
codingdogs <codingdogs@163.com>
2020-04-22 14:13:50 +0800

    upgrade

--- constant/urm_constant.go
+++ constant/urm_constant.go
@@ -1,6 +1,9 @@
 package constant
 
 const (
+	SMS_TPL_TYPE_NORMAL = 1 //普通短信
+	SMS_TPL_TYPE_AD     = 2 //广告
+
 	SMS_TPL_FAIL       = -1 //查询失败
 	SMS_TPL_AUDITING   = 0  //审核中
 	SMS_TPL_AUDIT_PASS = 1  //审核通过
