826fe0620cbc54b8e636666200fe5cfd2e6bea09
codingdogs <codingdogs@163.com>
2020-04-23 17:38:11 +0800

    添加constant

--- constant/merchant_constant.go
+++ constant/merchant_constant.go
@@ -2,4 +2,28 @@ package constant
 
 const (
 	MERCHANT_TYPE_WEIXIN = 1
+
+	MERCHANT_APPLY_STATUS_CREATED  = 1
+	MERCHANT_APPLY_STATUS_APPLYING = 2
+	MERCHANT_APPLY_STATUS_SUCCESS  = 3
+	MERCHANT_APPLY_STATUS_FAIL     = 4
 )
+
+var MERCHANT_APPLY_STATUS = []map[string]interface{}{
+	{
+		"label": "已提交",
+		"value": MERCHANT_APPLY_STATUS_CREATED,
+	},
+	{
+		"label": "申请中",
+		"value": MERCHANT_APPLY_STATUS_APPLYING,
+	},
+	{
+		"label": "申请成功",
+		"value": MERCHANT_APPLY_STATUS_SUCCESS,
+	},
+	{
+		"label": "申请失败",
+		"value": MERCHANT_APPLY_STATUS_FAIL,
+	},
+}
