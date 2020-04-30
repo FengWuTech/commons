b27ec686179af0e994629072c0b75f20329d1465
codingdogs <codingdogs@163.com>
2020-04-30 18:05:44 +0800

    upgrade

--- wss/command.go
+++ wss/command.go
@@ -1,10 +1,14 @@
 package wss
 
+import "time"
+
 const (
 	CHANNEL_NOTICE = "PMS_CHANNEL_NOTICE"
 
-	MESSAGE_TYPE_WELCOME    = 1
-	MESSAGE_TYPE_NEW_APPEAL = 2
+	MESSAGE_TYPE_WELCOME        = 1
+	MESSAGE_TYPE_NEW_APPEAL     = 2
+	MESSAGE_TYPE_NEW_BOOK_ORDER = 3
+	MESSAGE_TYPE_NEW_USER_AUDIT = 4
 )
 
 type Message struct {
@@ -20,4 +24,17 @@ type MessageWelcome struct {
 type MessageNewAppealOrder struct {
 	AppealID int    `json:"appeal_id"`
 	Detail   string `json:"detail"`
+}
+
+type MessageNewBook struct {
+	OrderID  int        `json:"order_id"`
+	ItemID   int        `json:"item_id"`
+	ItemName string     `json:"item_name"`
+	BookTime *time.Time `json:"book_time"`
+}
+
+type MessageNewUserAudit struct {
+	UserID       int    `json:"user_id"`
+	UserName     string `json:"user_name"`
+	RoomLocation string `json:"room_location"`
 }
