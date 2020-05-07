54e24f70fce953643bc0a915f98bf337d5403fed
codingdogs <codingdogs@163.com>
2020-05-07 11:51:16 +0800

    添加辅助函数

--- util/intutil/intutil.go
+++ util/intutil/intutil.go
@@ -1,5 +1,7 @@
 package intutil
 
+import "fmt"
+
 func InArray(num int, slice []int) bool {
 	for _, v := range slice {
 		if num == v {
@@ -21,4 +23,24 @@ func Intersect(sliceA []int, sliceB []int) []int {
 	}
 
 	return result
+}
+
+func ToInt(i interface{}) int {
+	switch i.(type) {
+	case int:
+		return i.(int)
+	case int8:
+		return int(i.(int8))
+	case int16:
+		return int(i.(int16))
+	case int32:
+		return int(i.(int32))
+	case int64:
+		return int(i.(int64))
+	case float32:
+		return int(i.(float32))
+	case float64:
+		return int(i.(float64))
+	}
+	panic(fmt.Sprintf("存在无法转换的数据类型: %v", i))
 }
