6295d86267ae0031ce98d3f6ac0fd560099b12de
codingdogs <codingdogs@163.com>
2020-05-08 17:10:09 +0800

    去掉配置

--- constant/weixin_template_message_const.go
+++ constant/weixin_template_message_const.go
@@ -1,56 +1,6 @@
 package constant
 
 const (
-
-	//物业管理通知
-	//{{first.DATA}}
-	//标题：{{keyword1.DATA}}
-	//发布时间：{{keyword2.DATA}}
-	//内容：{{keyword3.DATA}}
-	//{{remark.DATA}}
-	MINIP_STAFF_TEMPLATE_MESSAGE_MANAGE_NOTICE = "Uwsj-eyoeopnqLuJJlM-VpFTRdW_AD137BSrRQ2dbWU"
-
-	//工单超时
-	//{{first.DATA}}
-	//工单号：{{keyword1.DATA}}
-	//工单类别：{{keyword2.DATA}}
-	//工单状态：{{keyword3.DATA}}
-	//处理人员：{{keyword4.DATA}}
-	//具体内容：{{keyword5.DATA}}
-	//{{remark.DATA}}
-	MINIP_STAFF_TEMPLATE_MESSAGE_ORDER_TIMEOUT = "Ou0pvIyiy01wmQtsiZMAtSIvAe3Qf2j1Ah1-HSGGo0g"
-
-	//工单处理提醒
-	//{{first.DATA}}
-	//工单关联位置：{{keyword1.DATA}}
-	//工单编号：{{keyword2.DATA}}
-	//工单内容：{{keyword3.DATA}}
-	//操作人：{{keyword4.DATA}}
-	//时间：{{keyword5.DATA}}
-	//{{remark.DATA}}
-	MINIP_STAFF_TEMPLATE_MESSAGE_ORDER_REMIND = "D29wvzRRWpKQFW2o8Tge6mR9YewTsgQZNTPBKGqnKJk"
-
-	//报修处理结果
-	//{{first.DATA}}
-	//工单编号：{{keyword1.DATA}}
-	//房屋编号：{{keyword2.DATA}}
-	//维修主题：{{keyword3.DATA}}
-	//维修工程师：{{keyword4.DATA}}
-	//维修完成时间：{{keyword5.DATA}}
-	//{{remark.DATA}}
-	MINIP_STAFF_TEMPLATE_MESSAGE_ORDER_FINISH = "HsFB--XB-oZKKzb_ucIXGixIR6uPrXGjdlCQbqtc144"
-
-	//{{first.DATA}}
-	//联系人：{{keyword1.DATA}}
-	//联系电话：{{keyword2.DATA}}
-	//项目名称：{{keyword3.DATA}}
-	//工单内容：{{keyword4.DATA}}
-	//报修时间：{{keyword5.DATA}}
-	//{{remark.DATA}}
-	MINIP_STAFF_TEMPLATE_MESSAGE_ORDER_NEW = "kxoq3YpZky6zqhcJ0go-S2PC8s8KwuWTC5wbk6MnRz0"
-)
-
-const (
 	//{{first.DATA}}
 	//投票议题：{{keyword1.DATA}}
 	//投票内容：{{keyword2.DATA}}
