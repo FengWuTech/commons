cfcaa5f8a1601bbd69775af5da435a261cb50524
codingdogs <codingdogs@163.com>
2020-05-27 17:00:09 +0800

    修改转码逻辑

--- util/csvutil/csv.go
+++ util/csvutil/csv.go
@@ -12,6 +12,41 @@ func GBKToUtf8(src string) string {
 	return decoder.ConvertString(src)
 }
 
+func ParseCsvToMap2(content string, ignoreNum int, fromCharset string) []map[string]string {
+	if content == "" {
+		return nil
+	}
+
+	var titleItems []string
+	var dataList = make([]map[string]string, 0)
+
+	decoder := mahonia.NewDecoder(fromCharset)
+	content = decoder.ConvertString(content)
+
+	rows := strings.Split(content, "\n")
+	titleItems = strings.Split(rows[ignoreNum], ",")
+
+	for rowID := ignoreNum + 1; rowID < len(rows); rowID++ {
+		if rows[rowID] == "" {
+			continue
+		}
+		rowItems := strings.Split(rows[rowID], ",")
+		dataItem := make(map[string]string)
+		for k, v := range rowItems {
+			if k >= len(titleItems) {
+				continue
+			}
+			key := strings.TrimSpace(titleItems[k])
+			value := strings.TrimSpace(v)
+			value = strings.TrimLeft(value, "'")
+			dataItem[key] = value
+		}
+		dataList = append(dataList, dataItem)
+	}
+
+	return dataList
+}
+
 func ParseCSVToMap(content string, ignoreNum int) []map[string]string {
 	if content == "" {
 		return nil
@@ -20,7 +55,7 @@ func ParseCSVToMap(content string, ignoreNum int) []map[string]string {
 	var titleItems []string
 	var dataList = make([]map[string]string, 0)
 
-	if encodeutil.IsGBK([]byte(content)) {
+	if !encodeutil.IsUTF8([]byte(content)) {
 		content = GBKToUtf8(content)
 	}
 
