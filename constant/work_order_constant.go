45b1c1000439e324fec56ece74d92490f74d53ee
codingdogs <codingdogs@163.com>
2020-06-08 20:15:28 +0800

    修改常量

--- constant/work_order_constant.go
+++ constant/work_order_constant.go
@@ -46,12 +46,13 @@ const (
 	APPEAL_ORDER_STATUS_DISPATCHED = 3 //已分配
 	APPEAL_ORDER_STATUS_HANDLED    = 4 //已处理
 	APPEAL_ORDER_STATUS_FINISHED   = 6 //已结单
+	APPEAL_ORDER_STATUS_CANCELED   = 7 //已取消
 
 	DISPATCH_ORDER_STATUS_DISPATCHED = 1 //已派工
 	DISPATCH_ORDER_STATUS_ACCEPTED   = 2 //已接单
 	DISPATCH_ORDER_STATUS_REPAIRED   = 3 //已修复
 	DISPATCH_ORDER_STATUS_FINISHED   = 4 //已验证
-	DISPATCH_ORDER_STATUS_CANCEL     = 9 //已取消
+	DISPATCH_ORDER_STATUS_CANCELED   = 9 //已取消
 
 	DISPATCH_ORDER_OPERATE_TYPE_APPOINTMENT = 1 //修改预约
 	DISPATCH_ORDER_OPERATE_TYPE_FINISH      = 4 //完工
@@ -296,7 +297,12 @@ func GetOrderFeConf(key string) []map[string]interface{} {
 				"value": APPEAL_ORDER_STATUS_FINISHED,
 				"label": "已处理",
 			},
+			{
+				"value": APPEAL_ORDER_STATUS_CANCELED,
+				"label": "已关闭",
+			},
 		},
+
 		"DISPATCH_ORDER_STATUS": {
 			{
 				"value": DISPATCH_ORDER_STATUS_DISPATCHED,
@@ -315,8 +321,8 @@ func GetOrderFeConf(key string) []map[string]interface{} {
 				"label": "已完成",
 			},
 			{
-				"value": DISPATCH_ORDER_STATUS_CANCEL,
-				"label": "已取消",
+				"value": DISPATCH_ORDER_STATUS_CANCELED,
+				"label": "已关闭",
 			},
 		},
 		"RETURN_VISIT_STATUS": {
