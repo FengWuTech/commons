922baaa3e4487c2e8feb64362efc12a4092f657b
codingdogs <codingdogs@163.com>
2020-07-10 16:49:59 +0800

    添加常量

--- constant/urm_constant.go
+++ constant/urm_constant.go
@@ -31,4 +31,5 @@ const (
 	SMS_TPL_PASSWORD_RESET_NOTICE          = 60 //您的密码已被重置，新密码为:{1}
 	SMS_TPL_COMPANY_CREATE_PASSWORD_NOTICE = 61 //您被设置为锋物物管平台管理员，账号:{1} 密码:{2}
 	SMS_TPL_VISITOR_ARRIVED                = 63 //您邀请的访客{1}已经到访，请您关注。
+	SMS_TPL_STAFF_ADD_NOTICE               = 64 //您的员工账号为用户名:{1} 密码:{2}，请及时修改密码，避免密码泄露。
 )
